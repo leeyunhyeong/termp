3c0a912b2fb8030b23def8fe67cdc0df3080bf53
JustWildFlower <112952000+JustWildFlower@users.noreply.github.com>
2022-12-13 01:29:33 +0900

    Update termp.py

--- termp.py
+++ termp.py
@@ -30,5 +30,13 @@ if play==1:
         print("패배")
       
     if player=='보':
-
+      if computer == '가위':
+        print('컴퓨터는 가위를 냈습니다')
+        print('패배')
+      elif computer == '바위':
+        print("컴퓨터는 바위을 냈습니다")
+        print("승리")
+      elif computer == '보':
+        print("컴퓨터는 보를 냈습니다")
+        print("")
  
